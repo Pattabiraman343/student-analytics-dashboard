b1a6aa55c7d82d1c40d01519d442f7345bc7072b
Pattabi Raman <ramanpattabi843@gmail.com>
2026-03-14 15:13:14 +0530

    backend ready

--- server/server/routes/analyticsRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getAnalytics } from "../controllers/analyticsController.js";
-import { protect, allowRoles } from "../middleware/authMiddleware.js";
-
-const router = express.Router();
-
-router.get("/", protect, allowRoles("teacher"), getAnalytics);
-
-export default router;
